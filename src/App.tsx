4231207db6a628593cb633a618ba6676ddf6e8f7
dave-kwiatkowski <dave.kwiatkowski@uipath.com>
2022-06-07 09:03:42 -0700

    Change to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { FC } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { FC, StrictMode } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Page from './components/Page';
 import Shell from './components/Shell';
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <Shell>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/:slug' element={<Page />} />
-        </Routes>
-      </Shell>
-    </BrowserRouter>
+    <StrictMode>
+      <HashRouter>
+        <Shell>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/:slug' element={<Page />} />
+          </Routes>
+        </Shell>
+      </HashRouter>
+    </StrictMode>
   );
 };
 
