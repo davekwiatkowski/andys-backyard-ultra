70a8cce99ab485d9ff6be7c2dd2a45fda111a6a1
Dave Kwiatkowski <github@davekwiatkowski.com>
2022-06-11 15:52:01 -0700

    Cache sanity data

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,9 @@
-import { FC, useEffect, useMemo, useState } from 'react';
+import { FC, useMemo } from 'react';
 import imageUrlBuilder from '@sanity/image-url';
 import sanityClient from '../sanityClient';
 import BlockContent from '@sanity/block-content-to-react';
+import useSanityFetch from '../util/useSanityFetch';
+import LoadingSignal from './LoadingSignal';
 
 const builder = imageUrlBuilder(sanityClient);
 
@@ -10,43 +12,41 @@ function urlFor(source: string) {
 }
 
 const Page: FC = () => {
-  const [pageData, setPageData] = useState<any>(null);
   const sanityConfig = useMemo(() => sanityClient.config(), []);
-
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `*[_type == "landingPage"]{
-          mainImage {
-            asset-> {
-              _id,
-              url
-            }
-          },
-          content
-        }`,
-      )
-      .then(data => setPageData(data[0]))
-      .catch(console.error);
-  }, []);
-
-  if (!pageData) return <div>Loading...</div>;
+  const pageData = useSanityFetch(
+    `*[_type == "landingPage"]{
+      mainImage {
+        asset-> {
+          _id,
+          url
+        }
+      },
+      content
+    }`,
+    { isOneResult: true },
+  );
 
   return (
-    <div className='flex max-w-full flex-row flex-wrap gap-4'>
-      <img
-        src={urlFor(pageData.mainImage).width(500).url()}
-        className='h-fit object-cover'
-        alt='Field with tents at the event'
-      />
-      <div className='max-w-lg pb-8 xl:max-w-2xl'>
-        <BlockContent
-          className='block-content'
-          blocks={pageData.content}
-          projectId={sanityConfig.projectId}
-          dataset={sanityConfig.dataset}
-        />
-      </div>
+    <div className='flex flex-row flex-wrap max-w-full gap-4'>
+      {pageData ? (
+        <>
+          <img
+            src={urlFor(pageData.mainImage).width(500).url()}
+            className='object-cover h-fit'
+            alt='Field with tents at the event'
+          />
+          <div className='max-w-lg pb-8 xl:max-w-2xl'>
+            <BlockContent
+              className='block-content'
+              blocks={pageData.content}
+              projectId={sanityConfig.projectId}
+              dataset={sanityConfig.dataset}
+            />
+          </div>
+        </>
+      ) : (
+        <LoadingSignal />
+      )}
     </div>
   );
 };
--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,29 +1,21 @@
-import { FC, ReactNode, useEffect, useMemo, useState } from 'react';
+import { FC, ReactNode, useMemo, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import sanityClient from '../sanityClient';
+import useSanityFetch from '../util/useSanityFetch';
+import LoadingSignal from './LoadingSignal';
 
 const Shell: FC<{ children?: ReactNode }> = ({ children }) => {
   const location = useLocation();
-  const [allPagesData, setAllPagesData] = useState<any>(null);
   const [isExpanded, setIsExpanded] = useState(false);
   const currentPath = useMemo(
     () => location.pathname.substring(1),
     [location.pathname],
   );
-
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `*[_type == "page"] {
-          title,
-          slug
-        }`,
-      )
-      .then(data => setAllPagesData(data))
-      .catch(console.error);
-  }, []);
-
-  if (!allPagesData) return <div>Loading...</div>;
+  const allPagesData = useSanityFetch(
+    `*[_type == "page"] {
+      title,
+      slug
+    }`,
+  );
 
   return (
     <div className='font-serif'>
@@ -31,7 +23,7 @@ const Shell: FC<{ children?: ReactNode }> = ({ children }) => {
         className={`fixed z-50 flex min-h-[72px] w-full flex-wrap items-center justify-between p-6 md:bg-red-50 md:shadow-sm ${
           isExpanded ? 'bg-red-100 shadow-lg' : 'bg-red-50 shadow-sm'
         }`}>
-        <div className='mr-6 flex flex-shrink-0 items-center'>
+        <div className='flex items-center flex-shrink-0 mr-6'>
           <Link
             to='/'
             className='text-xl font-semibold tracking-tight'
@@ -41,7 +33,7 @@ const Shell: FC<{ children?: ReactNode }> = ({ children }) => {
             Andy's Backyard Ultra
           </Link>
           {currentPath && (
-            <span className='text-xs capitalize italic text-gray-500 md:hidden'>
+            <span className='text-xs italic text-gray-500 capitalize md:hidden'>
               <span className='pl-2 pr-2'>{'>'}</span>
               <span>{currentPath}</span>
             </span>
@@ -49,12 +41,12 @@ const Shell: FC<{ children?: ReactNode }> = ({ children }) => {
         </div>
         <div className='block md:hidden'>
           <button
-            className='flex items-center border border-black px-3 py-2 text-black hover:border-black hover:bg-black hover:text-white'
+            className='flex items-center px-3 py-2 text-black border border-black hover:border-black hover:bg-black hover:text-white'
             onClick={() => {
               setIsExpanded(!isExpanded);
             }}>
             <svg
-              className='h-3 w-3 fill-current'
+              className='w-3 h-3 fill-current'
               viewBox='0 0 20 20'
               xmlns='http://www.w3.org/2000/svg'>
               <title>Menu</title>
@@ -67,21 +59,25 @@ const Shell: FC<{ children?: ReactNode }> = ({ children }) => {
             isExpanded ? 'visible' : 'hidden'
           } md:visible md:flex md:w-auto md:items-center`}>
           <div className='md:flex-grow'>
-            {allPagesData.map((page: any) => (
-              <Link
-                onClick={() => {
-                  setIsExpanded(false);
-                }}
-                className={`mt-4 mr-4 block text-gray-500 hover:text-black md:mt-0 md:inline-block ${
-                  currentPath === page.slug.current
-                    ? 'underline'
-                    : 'no-underline'
-                }`}
-                to={`/${page.slug.current}`}
-                key={page.slug.current}>
-                {page.title}
-              </Link>
-            ))}
+            {allPagesData ? (
+              allPagesData.map((page: any) => (
+                <Link
+                  onClick={() => {
+                    setIsExpanded(false);
+                  }}
+                  className={`mt-4 mr-4 block text-gray-500 hover:text-black md:mt-0 md:inline-block ${
+                    currentPath === page.slug.current
+                      ? 'underline'
+                      : 'no-underline'
+                  }`}
+                  to={`/${page.slug.current}`}
+                  key={page.slug.current}>
+                  {page.title}
+                </Link>
+              ))
+            ) : (
+              <LoadingSignal />
+            )}
           </div>
         </div>
       </nav>
